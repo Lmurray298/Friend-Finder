264bdde65fa704cad90404e1eedf05bf7ab56491
Liam Murray <lmurray298@Liams-MacBook-Pro.local>
2019-03-22 21:39:44 -0400

    fixed errors

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var friendsArray = [
+	{
+		name: 'Ron',
+		photo: 'https://www.google.com/search?rlz=1C5CHFA_enUS758US758&biw=1596&bih=832&tbm=isch&sa=1&ei=YIWVXNnlFY6o_QbTyLaADA&q=ron+burgundy+jpeg&oq=ron+burgundy+jpeg&gs_l=img.3...704.2874..3135...0.0..0.178.576.2j3......1....1..gws-wiz-img.......0i67j0.knByJN_d8G8#imgrc=w8b1fdy0holthM:',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Frank',
+		photo: 'https://www.google.com/search?hl=en&biw=1596&bih=832&tbm=isch&sa=1&ei=xIWVXPbJOsi2ggeMz7SAAg&q=frank+gallagher+shameless&oq=frank+gallagher+shamele&gs_l=img.1.0.0l8j0i8i30l2.1755.10568..11793...1.0..0.157.1172.8j4......1....1..gws-wiz-img.lwFuEuaGDYY#imgrc=VegvV2S_rDyzPM:',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	
+];
+
+module.exports = friendsArray;
